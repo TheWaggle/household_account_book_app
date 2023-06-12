5a2811bb2798c4049b1b806e83877085e54dc689
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2023-06-12 17:23:35 +0900

    HouseholdAccountBookControllerとテンプレートに日付（月）検索機能を追加

--- lib/household_account_book_app_web/controllers/household_account_book_controller.ex
+++ lib/household_account_book_app_web/controllers/household_account_book_controller.ex
@@ -4,6 +4,28 @@ defmodule HouseholdAccountBookAppWeb.HouseholdAccountBookController do
   alias HouseholdAccountBookApp.Incomes
   alias HouseholdAccountBookApp.Purchases
 
+  def summary(conn, %{"date" => date}) do
+    [year, month] =
+      date
+      |> String.split("-")
+      |> Enum.map(fn d -> String.to_integer(d) end)
+
+    date = Date.new!(year, month, 1)
+
+    sum_incomes = Incomes.sum_incomes_by_month(date)
+    money_by_categories = Purchases.get_money_by_categories(date)
+    money_by_date = Purchases.get_money_by_date(date)
+
+    render(
+      conn,
+      :summary,
+      sum_incomes: sum_incomes,
+      money_by_categories: money_by_categories,
+      money_by_date: money_by_date,
+      date: date
+    )
+  end
+
   def summary(conn, _params) do
     date = Date.utc_today()
 
